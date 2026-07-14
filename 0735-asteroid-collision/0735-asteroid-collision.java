6d04d8df5a54f68c7d6668cbb61e5d16079e56da
dEE rA <rahuljoshi50009@gmail.com>
2026-07-14 18:38:12 +0530

    Time: 5 ms (69.38%), Space: 46.8 MB (72.54%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> st = new Stack<>();
+
+        for (int a : asteroids) {
+
+            boolean destroyed = false;
+
+            while (!st.isEmpty() && st.peek() > 0 && a < 0) {
+
+                if (st.peek() < -a) {
+                    st.pop();
+                } 
+                else if (st.peek() == -a) {
+                    st.pop();
+                    destroyed = true;
+                    break;
+                } 
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                st.push(a);
+            }
+        }
+
+        int[] ans = new int[st.size()];
+
+        for (int i = ans.length - 1; i >= 0; i--) {
+            ans[i] = st.pop();
+        }
+
+        return ans;
+    }
+}
