b74a93fc830258b6ea68540bc747e5c33cfd6e0a
dEE rA <rahuljoshi50009@gmail.com>
2026-07-17 16:46:10 +0530

    Time: 4 ms (98.85%), Space: 46.1 MB (28.87%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (char ch : moves.toCharArray()) {
+            switch (ch) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
