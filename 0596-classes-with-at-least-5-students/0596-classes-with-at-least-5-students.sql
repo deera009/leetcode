78691169cc12c31ca992b748001c346eb137f167
dEE rA <rahuljoshi50009@gmail.com>
2026-07-23 17:00:12 +0530

    Time: 344 ms (45.22%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-SELECT class FROM Courses GROUP BY class HAVING COUNT(student) >= 5;
+SELECT class FROM Courses 
+GROUP BY class HAVING COUNT(student) >= 5;
