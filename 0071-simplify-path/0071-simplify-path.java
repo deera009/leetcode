432dbb4ba65a60223716b7eea74e77bbd0fd2e9a
dEE rA <rahuljoshi50009@gmail.com>
2026-07-28 17:11:07 +0530

    Time: 6 ms (28.28%), Space: 45.3 MB (22.83%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new ArrayDeque<>();
+
+        String[] parts = path.split("/");
+
+        for (String part : parts) {
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder ans = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+            ans.insert(0, "/" + stack.pop());
+        }
+
+        return ans.toString();
+    }
+}
