eeb00af9c4e4fef9c9892877244eaf64da6475de
dEE rA <rahuljoshi50009@gmail.com>
2026-08-14 21:05:03 +0530

    Time: 1 ms (77.64%), Space: 42.8 MB (11.85%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> visited = new HashSet<>();
+
+        while (n != 1 && !visited.contains(n)) {
+            visited.add(n);
+            n = getNextNumber(n);
+        }
+
+        return n == 1;
+    }
+
+    private int getNextNumber(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
