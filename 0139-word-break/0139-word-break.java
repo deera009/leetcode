47019327036b51bfbf8be6520b085d4785213793
dEE rA <rahuljoshi50009@gmail.com>
2026-08-15 21:49:07 +0530

    Time: 8 ms (49.87%), Space: 46.2 MB (48.43%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        Set<String> dictionary = new HashSet<>(wordDict);
+
+        int n = s.length();
+
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 0; j < i; j++) {
+
+                if (dp[j]
+                        && dictionary.contains(s.substring(j, i))) {
+
+                    dp[i] = true;
+
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
